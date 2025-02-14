0cd04f217ee30eda469ec88e3bb4bad3df604f2b
KridYou <krid.s@kanda.digital>
2025-02-14 17:27:56 +0700

    set up docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      driver: require('mysql2'),
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USER || 'nestuser',
+      password: process.env.DB_PASSWORD || 'nestpass',
+      database: process.env.DB_NAME || 'authen-card-db',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
